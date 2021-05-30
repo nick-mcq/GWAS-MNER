0c59c944f3685e037cfeecd07775d06bb8aa7401
nick-mcq <nicholas_mcquibban@hotmail.com>
2021-05-30 01:00:08 +0100

    Added full assay model and updated streamlit app

---
+++ branch_models/assay_simple/assay_data_exclusion.py
@@ -0,0 +1,264 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 25 12:54:36 2021
+
+@author: nicholasmcquibban
+"""
+
+
+import os
+import regex as re
+import datetime
+
+
+import pandas as pd
+from tqdm import tqdm
+import spacy
+from spacy.tokens import DocBin
+
+
+start_time = datetime.datetime.now() #grabs start time
+
+message = '' #empty string to hold message with time in it
+
+PMC_files=[] #empty list to hold all PMC filenames in it for iteration
+
+all_files = os.listdir() #make sure to run this algorithm in the directory containing the PMCID.txt files
+
+for n in all_files:
+    
+    if n.startswith('PMC'):  #gets a list of every PMC file in the directory
+        PMC_files.append(n)
+
+content = ()
+
+tuple_list = []
+
+all_sentences = []
+
+for in_file in PMC_files: #for each PMC M&M file do
+
+    with open(in_file) as m_file:
+        
+        data = m_file.read()
+       
+        illumina_sentence = re.findall(r"([^.]*?Illumina[^.]*\d.\d*[^.]*.)", data)
+        
+        all_sentences += illumina_sentence
+        
+        affymetrix_sentence = re.findall(r"([^.]*?Affymetrix[^.]*\d.\d*[^.]*.)", data)
+        
+        all_sentences += affymetrix_sentence
+        
+        perlegen_sentence = re.findall(r"([^.]*?Perlegen[^.]*\d.\d*[^.]*.)", data)
+        
+        all_sentences += perlegen_sentence
+        
+#all_sentences = impute_sentences + beagle_sentences + mach_sentences
+
+        #content.append()
+        
+        
+#print(all_sentences)
+
+all_sentences_nodupes = list(set(all_sentences))
+
+#print(all_sentences_nodupes)
+
+with open('TEST2_DATA', 'a') as out_file:
+    
+    #/project/home20/nam220/NER/NLP/
+    
+    for n in all_sentences_nodupes:
+        
+        if " or " in n:
+            
+            continue
+        
+        if " Array Sets " in n:
+            
+            continue
+        
+        if "(Illumina)" in n:
+            
+            continue
+        
+        if "(Affymetrix)" in n:
+            
+            continue
+        
+        if "(Supplementary Table 4)" in n:
+            
+            continue
+        
+        if "Illumina" and "Affymetrix" in n:
+            
+            continue
+        
+        entity_list = []
+        
+        match_Iplat = re.search(r"(Illumina.*?platform[s]?)", n, re.IGNORECASE)
+        match_IaN = re.search(r"(Illumina.*?array[s]?.\d[.]?\d)", n, re.IGNORECASE)
+        match_Ia = re.search("(Illumina.*?array[s]?)", n, re.IGNORECASE)
+        match_Ic = re.search(r"(Illumina.*? chip[s]?)", n, re.IGNORECASE)
+        match_IBC = re.search(r"(Illumina.*?beadchip[s]?)", n, re.IGNORECASE)
+        match_IK = re.search(r"(Illumina.\d+.?[K])", n, re.IGNORECASE)
+        match_IW = re.search(r"(Illumina.\d+.?[W])", n, re.IGNORECASE)
+        
+        
+        match_Ac = re.search(r"(Affymetrix.*? chip[s]?)", n, re.IGNORECASE)
+        match_AaN = re.search(r"(Affymetrix.*?array[s]?.\d[.]?\d)", n, re.IGNORECASE)
+        match_Aa = re.search(r"(Affymetrix.*?array[s]?)", n, re.IGNORECASE)
+        match_Aplat = re.search(r"(Affymetrix.*?platform[s]?)", n, re.IGNORECASE)
+        match_AK = re.search(r"(Affymetrix.\d+.?[K])", n, re.IGNORECASE)
+        
+        match_Pc = re.search(r"(Perlegen.*? chip[s]?)", n, re.IGNORECASE)
+        match_PK = re.search(r"(Perlegen.\d+.?[K])", n, re.IGNORECASE)
+        
+        
+        
+        match_V = re.search(r"(v\d+[.]?\d+)", n, re.IGNORECASE)
+        
+        
+        if match_Iplat:
+            #print(match)
+            
+            info = (match_Iplat.start(), match_Iplat.end(), 'Illumina assay')
+            entity_list.append(info)
+            
+        elif match_IBC:
+            
+            info = (match_IBC.start(), match_IBC.end(), 'Illumina assay')
+            entity_list.append(info)
+        
+        
+        elif match_IaN:
+            
+            info = (match_IaN.start(), match_IaN.end(), 'Illumina assay')
+            entity_list.append(info)
+            
+            
+        elif match_Ia:
+            
+            info = (match_Ia.start(), match_Ia.end(), 'Illumina assay')
+            entity_list.append(info)
+        
+        elif match_Ic:
+            
+            info = (match_Ic.start(), match_Ic.end(), 'Illumina assay')
+            entity_list.append(info)
+        
+        
+        elif match_IK:
+            
+            info = (match_IK.start(), match_IK.end(), 'Illumina assay')
+            entity_list.append(info)
+            
+        elif match_IW:
+            
+            info = (match_IW.start(), match_IW.end(), 'Illumina assay')
+            entity_list.append(info)
+        
+        if match_Ac:
+            
+            info = (match_Ac.start(), match_Ac.end(), 'Affymetrix assay')
+            entity_list.append(info)
+            
+        elif match_AaN:
+            
+            info = (match_AaN.start(), match_AaN.end(), 'Affymetrix assay')
+            entity_list.append(info)
+            
+        elif match_Aa:
+            
+            info = (match_Aa.start(), match_Aa.end(), 'Affymetrix assay')
+            entity_list.append(info)
+            
+        elif match_Aplat:
+            
+            info = (match_Aplat.start(), match_Aplat.end(), 'Affymetrix assay')
+            entity_list.append(info)
+        
+        elif match_AK:
+            
+            info = (match_AK.start(), match_AK.end(), 'Affymetrix assay')
+            entity_list.append(info)
+            
+            
+        if match_Pc:
+            
+            info = (match_Pc.start(), match_Pc.end(), 'Perlegen assay')
+            entity_list.append(info)
+            
+        elif match_PK:
+            
+            info = (match_PK.start(), match_PK.end(), 'Perlegen assay')
+            entity_list.append(info)
+                      
+        
+        content = (n, {'entities': entity_list})
+            
+        tuple_list.append(content)
+        
+
+    print(tuple_list)        
+
+    
+    for element in tuple_list:
+        
+        if element == tuple_list[0]:
+            
+            out_file.write('[')
+            out_file.write(str(element))
+            out_file.write(',')
+            out_file.write('\n')
+            
+        elif element == tuple_list[-1]:
+            
+            out_file.write(' ')
+            out_file.write(str(element))
+            out_file.write(']')
+        
+        else:
+       
+            out_file.write(' ')
+            out_file.write(str(element))
+            out_file.write(',')
+            out_file.write('\n')
+       
+ 
+    #print(content[0])
+
+#('Full sentence appears here.', {'entities': [(start, stop, 'platform')]})
+   
+ 
+nlp = spacy.blank("en") # load a new spacy model
+db = DocBin() # create a DocBin object
+
+DEV_DATA = tuple_list
+
+for text, annot in tqdm(DEV_DATA): # data in previous format
+    doc = nlp.make_doc(text) # create doc object from text
+    ents = []
+    for start, end, label in annot["entities"]: # add character indexes
+        span = doc.char_span(start, end, label=label, alignment_mode="contract")
+        if span is None:
+            print("Skipping entity")
+        else:
+            ents.append(span)
+    doc.ents = ents # label the text with the ents
+    db.add(doc)
+
+db.to_disk("/project/home20/nam220/NER/NLP/test.spacy") # save the docbin object
+
+#"/project/home20/nam220/NER/NLP/train.spacy"
+
+stop_time = datetime.datetime.now() #grabs stop time
+
+message = 'Start time is ' + str(start_time) + '\n' + 'Stop time is ' + str(stop_time)  
+
+with open('/project/home20/nam220/NER/NLP/TEST2_DATA_runtime.log', 'a') as time_file:   #start and stop time are written into a runtime.log file
+    
+    #'/project/home20/nam220/NER/NLP/
+    time_file.write(message)
--- streamlit_visualiser.py
+++ streamlit_visualiser.py
@@ -75,13 +75,57 @@ def main():
     elif choice == "NER":
       st.subheader("Named Entity Recognition")
       # Add a selectbox to the sidebar:
-      sel = st.sidebar.selectbox("Which entity would you like to identify?", ["Platform"])
+      sel = st.sidebar.selectbox("Which entity would you like to identify?", ["Platform", "Imputation", 'Total SNPs', 'Quality Control', 'Assays'])
       if sel=="Platform":
          #path=model_loader("https://github.com/fm1320/IC_NLP/releases/download/V3/V3-20210203T001829Z-001.zip", "V3")   
-         nlp = spacy.load('./output/model-best') #this will change based on which choice was made
-      raw_text = st.text_area("Enter text for entity recognition","Total RNA from the middle temporal cortex (Brodmann areas 20 and 21) from 86 subjects was isolated and randomly hybridized to Affymetrix Human Exon 1.0 ST arrays, and quality control analysis was performed using standard methods. The effects of several methodological (day of expression hybridization, RNA integrity number (RIN)) and biological covariates (sex, age and medication) on exon–gene expression relationships were tested for significance. Of these individuals, 71 had participated in a published epilepsy genome-wide association study, and, therefore, genotyping data were available. Details of sample collection and genotyping quality control steps have been published previously66. These samples were assayed with Illumina HumanHap550v3 (N = 44) and Illumina Human610-Quadv1 (N = 27) arrays.")   
-      docx = nlp(raw_text)
-      spacy_streamlit.visualize_ner(docx,labels=nlp.get_pipe('ner').labels)  
+         nlp1 = spacy.load('/Users/nicholasmcquibban/Desktop/branch_outputs/platform/output/model-best') #this will change based on which choice was made
+         raw_text = st.text_area("Enter text for entity recognition","Total RNA from the middle temporal cortex (Brodmann areas 20 and 21) from 86 subjects was isolated and randomly hybridized to Affymetrix Human Exon 1.0 ST arrays, and quality control analysis was performed using standard methods. The effects of several methodological (day of expression hybridization, RNA integrity number (RIN)) and biological covariates (sex, age and medication) on exon–gene expression relationships were tested for significance. Of these individuals, 71 had participated in a published epilepsy genome-wide association study, and, therefore, genotyping data were available. Details of sample collection and genotyping quality control steps have been published previously66. These samples were assayed with Illumina HumanHap550v3 (N = 44) and Illumina Human610-Quadv1 (N = 27) arrays.")   
+         docx = nlp1(raw_text)
+         spacy_streamlit.visualize_ner(docx,labels=nlp1.get_pipe('ner').labels) 
+     
+      if sel=="Imputation":
+         #path=model_loader("https://github.com/fm1320/IC_NLP/releases/download/V3/V3-20210203T001829Z-001.zip", "V3")   
+         nlp2 = spacy.load('/Users/nicholasmcquibban/Desktop/branch_outputs/imputation_negation/output/model-best') #this will change based on which choice was made
+         raw_text = st.text_area("Enter text for entity recognition","To validate additional associated SNPs (p < 0.0001) and nominally associated candidate genes, we imputed SNPs from our GWAS using a previously published GWAS1 and the IMPUTE program. ")   
+         docx = nlp2(raw_text)
+         spacy_streamlit.visualize_ner(docx,labels=nlp2.get_pipe('ner').labels) 
+         
+      if sel=="Total SNPs":
+         #path=model_loader("https://github.com/fm1320/IC_NLP/releases/download/V3/V3-20210203T001829Z-001.zip", "V3")   
+         nlp3 = spacy.load('/Users/nicholasmcquibban/Desktop/branch_outputs/SNP#/output/model-best') #this will change based on which choice was made
+         raw_text = st.text_area("Enter text for entity recognition","We based our further analyses on 2,168,847 SNPs that met imputation and genotyping QC criteria across all studies (Methods; Supplementary Methods). We then conducted a series of association analyses to relate the 2.2 million genotyped and/or imputed SNPs with plasma concentrations of HDL cholesterol, LDL cholesterol and triglyceride concentrations. ")   
+         docx = nlp3(raw_text)
+         spacy_streamlit.visualize_ner(docx,labels=nlp3.get_pipe('ner').labels) 
+         
+      if sel=="Quality Control":
+         #path=model_loader("https://github.com/fm1320/IC_NLP/releases/download/V3/V3-20210203T001829Z-001.zip", "V3")   
+         nlp4 = spacy.load('/Users/nicholasmcquibban/Desktop/branch_outputs/QC/output/model-best') #this will change based on which choice was made
+         raw_text = st.text_area("Enter text for entity recognition","Quality control of genotype data and genetic association analyses were performed using PLINK v1.07 ().")   
+         docx = nlp4(raw_text)
+         spacy_streamlit.visualize_ner(docx,labels=nlp4.get_pipe('ner').labels) 
+         
+      if sel=="Assays":
+         #path=model_loader("https://github.com/fm1320/IC_NLP/releases/download/V3/V3-20210203T001829Z-001.zip", "V3")   
+         nlp5 = spacy.load('/Users/nicholasmcquibban/Desktop/branch_outputs/assay_total/output/model-best') #this will change based on which choice was made
+         raw_text = st.text_area("Enter text for entity recognition","The 529 LCLs derived from the CAP cohort were incubated under standardized conditions for 24hr, after which MGMT transcript levels were quantified using the Illumina H8v3 beadarray (Illumina probe ILMN_1795639). Each of the studies had been conducted on different platforms (Illumina 550K for CHOP, Perlegen 600K for IMAGE, Affymetrix 500K for IMAGE II, and Illumina 1M Duo platform.")   
+         docx = nlp5(raw_text)
+         spacy_streamlit.visualize_ner(docx,labels=nlp5.get_pipe('ner').labels) 
     
 if __name__ == '__main__':
     main()
+    
+    
+    
+    
+    
+    
+    #text for platform: Total RNA from the middle temporal cortex (Brodmann areas 20 and 21) from 86 subjects was isolated and randomly hybridized to Affymetrix Human Exon 1.0 ST arrays, and quality control analysis was performed using standard methods. The effects of several methodological (day of expression hybridization, RNA integrity number (RIN)) and biological covariates (sex, age and medication) on exon–gene expression relationships were tested for significance. Of these individuals, 71 had participated in a published epilepsy genome-wide association study, and, therefore, genotyping data were available. Details of sample collection and genotyping quality control steps have been published previously66. These samples were assayed with Illumina HumanHap550v3 (N = 44) and Illumina Human610-Quadv1 (N = 27) arrays.
+    #text for imputation: To validate additional associated SNPs (p < 0.0001) and nominally associated candidate genes, we imputed SNPs from our GWAS using a previously published BEAGLE GWAS1 and the IMPUTE program. 
+    
+    
+    
+    
+    
+    
+    
+    
