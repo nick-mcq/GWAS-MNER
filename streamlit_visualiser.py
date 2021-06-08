7628ea6fdc038bc63d9fb871a876e229a9a9efed
nick-mcq <nicholas_mcquibban@hotmail.com>
2021-06-08 16:53:34 +0100

    Update streamlit_visualiser.py

--- streamlit_visualiser.py
+++ streamlit_visualiser.py
@@ -187,7 +187,7 @@ def main():
       sel = st.sidebar.selectbox("Which entity would you like to identify?", ["Platform", "Imputation", 'Total SNPs', 'Quality Control', 'Assays'])
       if sel=="Platform":
          #path=model_loader("https://github.com/fm1320/IC_NLP/releases/download/V3/V3-20210203T001829Z-001.zip", "V3")   
-         nlp1 = spacy.load('/Users/nicholasmcquibban/Desktop/branch_outputs/platform/output/model-best') #this will change based on which choice was made
+         nlp1 = spacy.load('platform/output/model-best') #this will change based on which choice was made
          raw_text = st.text_area("Enter text for entity recognition","Total RNA from the middle temporal cortex (Brodmann areas 20 and 21) from 86 subjects was isolated and randomly hybridized to Affymetrix Human Exon 1.0 ST arrays, and quality control analysis was performed using standard methods. The effects of several methodological (day of expression hybridization, RNA integrity number (RIN)) and biological covariates (sex, age and medication) on exon–gene expression relationships were tested for significance. Of these individuals, 71 had participated in a published epilepsy genome-wide association study, and, therefore, genotyping data were available. Details of sample collection and genotyping quality control steps have been published previously66. These samples were assayed with Illumina HumanHap550v3 (N = 44) and Illumina Human610-Quadv1 (N = 27) arrays.", help="Sample text source: Stein, J.L., Medland, S.E., Vasquez, A.A., Hibar, D.P., et al. (2012)")   
          docx = nlp1(raw_text)
          spacy_streamlit.visualize_ner(docx,labels=nlp1.get_pipe('ner').labels) 
@@ -214,7 +214,7 @@ def main():
              
       if sel=="Imputation":
          #path=model_loader("https://github.com/fm1320/IC_NLP/releases/download/V3/V3-20210203T001829Z-001.zip", "V3")   
-         nlp2 = spacy.load('/Users/nicholasmcquibban/Desktop/branch_outputs/imputation_negation/output/model-best') #this will change based on which choice was made
+         nlp2 = spacy.load('imputation_negation/output/model-best') #this will change based on which choice was made
          raw_text = st.text_area("Enter text for entity recognition","To validate additional associated SNPs (p < 0.0001) and nominally associated candidate genes, we imputed SNPs from our GWAS using a previously published GWAS1 along with both the IMPUTE and BEAGLE programs. In addition another study, which did not impute any further SNPs, served to further validate our results for candidate genes (see Supplementary Table 5) ")   
          docx = nlp2(raw_text)
          spacy_streamlit.visualize_ner(docx,labels=nlp2.get_pipe('ner').labels) 
@@ -241,7 +241,7 @@ def main():
          
       if sel=="Total SNPs":
          #path=model_loader("https://github.com/fm1320/IC_NLP/releases/download/V3/V3-20210203T001829Z-001.zip", "V3")   
-         nlp3 = spacy.load('/Users/nicholasmcquibban/Desktop/branch_outputs/SNP#/output/model-best') #this will change based on which choice was made
+         nlp3 = spacy.load('SNP#/output/model-best') #this will change based on which choice was made
          raw_text = st.text_area("Enter text for entity recognition","We based our further analyses on 2,168,847 SNPs that met imputation and genotyping QC criteria across all studies (Methods; Supplementary Methods). We then conducted a series of association analyses to relate the 2.2 million genotyped and/or imputed SNPs with plasma concentrations of HDL cholesterol, LDL cholesterol and triglyceride concentrations. ")   
          docx = nlp3(raw_text)
          spacy_streamlit.visualize_ner(docx,labels=nlp3.get_pipe('ner').labels) 
@@ -268,7 +268,7 @@ def main():
          
       if sel=="Quality Control":
          #path=model_loader("https://github.com/fm1320/IC_NLP/releases/download/V3/V3-20210203T001829Z-001.zip", "V3")   
-         nlp4 = spacy.load('/Users/nicholasmcquibban/Desktop/branch_outputs/QC/output/model-best') #this will change based on which choice was made
+         nlp4 = spacy.load('QC/output/model-best') #this will change based on which choice was made
          raw_text = st.text_area("Enter text for entity recognition","Quality control of genotype data and genetic association analyses were performed using PLINK v1.07 ().")   
          docx = nlp4(raw_text)
          spacy_streamlit.visualize_ner(docx,labels=nlp4.get_pipe('ner').labels) 
@@ -294,7 +294,7 @@ def main():
          
       if sel=="Assays":
          #path=model_loader("https://github.com/fm1320/IC_NLP/releases/download/V3/V3-20210203T001829Z-001.zip", "V3")   
-         nlp5 = spacy.load('/Users/nicholasmcquibban/Desktop/branch_outputs/assay_total/output/model-best') #this will change based on which choice was made
+         nlp5 = spacy.load('assay_total/output/model-best') #this will change based on which choice was made
          raw_text = st.text_area("Enter text for entity recognition","The 529 LCLs derived from the CAP cohort were incubated under standardized conditions for 24hr, after which MGMT transcript levels were quantified using the Illumina H8v3 beadarray. Individuals in the WHI-SHARe cohort were genotyped on the Affymetrix 6.0 array.")   
          docx = nlp5(raw_text)
          spacy_streamlit.visualize_ner(docx,labels=nlp5.get_pipe('ner').labels) 
